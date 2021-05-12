39f950e49dce9ab7f7177b4b67b7a39c393dffa8
Barry Demchak <idekerlab.bdemchak@gmail.com>
2021-05-11 18:33:20 -0700

    test

--- doc/conf.py
+++ doc/conf.py
@@ -99,10 +99,10 @@ language = None
 #
 # [BD] Alabaster is the default, but is too white, so I commented out: html_theme = 'alabaster'
 # Note that this means we can also comment out certain CSS (located in _static/css)
-# import sphinx_rtd_theme
+import sphinx_rtd_theme
 # html_theme = "sphinx_rtd_theme"
-html_theme = "classic" # "sphinx_rtd_theme" # "nature" # "alabaster" # "sphinx_rtd_theme" #"classic"
-# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "sphinx_rtd_theme" # "nature" # "alabaster" # "sphinx_rtd_theme" #"classic"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # [BD] from http://stackoverflow.com/questions/32079200/how-do-i-set-up-custom-styles-for-restructuredtext-sphinx-readthedocs-etc/32079202#32079202
 ##html_logo = '_static/images/cytoscape3-icon-trans-128x128.png'
 ##html_favicon = '_static/images/cytoscape3-icon.ico'
